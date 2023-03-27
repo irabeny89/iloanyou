315f47a664fb3b83672884df0a069941fb7f62a0
Irabeny <58922232+irabeny89@users.noreply.github.com>
2023-03-27 15:18:23 +0100

    tests: add sample test for hello query resolver

---
+++ src/test/sample.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "@jest/globals";
+import resolvers from "../gql/resolvers";
+
+describe("Sample GraphQL hello query", () => {
+  it("should return hi", () => {
+    expect(resolvers.Query.hello()).toBe("hi");
+  });
+});
